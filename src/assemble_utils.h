503124ce51f26d3f9dd9ac635339e12458e429db
sea19 <sea19@ic.ac.uk>
2020-06-18 16:58:42 +0100

    Christopher's extension should be working as well

--- src/assemble_utils.h
+++ src/assemble_utils.h
@@ -2,9 +2,16 @@
 #define ASSEMBLE_UTILS_H
 #include <stdio.h>
 
+static const int undefined = -436207600; //11100110000000000000000000010000
+
 enum mnemonics {
-	ADD, SUB, RSB, AND, EOR, ORR, MOV, TST, TEQ, CMP, MUL, MLA, LDR, STR, BEQ, BNE, BGE, BLT, BGT, BLE, B, LSLOP, ANDEQ
+	ADD, SUB, RSB, AND, EOR, ORR, MOV, TST, TEQ, CMP, MUL, MLA, LDR, STR,
+	BEQ, BNE, BGE, BLT, BGT, BLE, B,
+	BLEQ, BLNE, BLGE, BLLT, BLGT, BLLE, BL,	//extension instruction set
+	LSL, ANDEQ, SWI, BX, SWP, SWPB, UNDEFINED	//swi, bx, swp, swpb - extension instructions
+
 };
+
 
 int findLabel(FILE *srcFile, char *label, int currentAddress);
 
@@ -23,6 +30,8 @@ int assembleMultiply(enum mnemonics mnemonic, char operands[6][20]);
 int assembleSingleDataTransfer(enum mnemonics mnemonic, char operands[6][20], int *storedValues, int noOfInstructions, int pc);
 
 int assembleBranch(enum mnemonics mnemonic, char operands[6][20], FILE *srcFile, int currentAddress);
+
+int assembleBranchLink(enum mnemonics mnemonic, char operands[6][20], FILE *srcFile, int currentAddress);
 
 int assembleSpecial(enum mnemonics mnemonic, char operands[6][20]);
 
